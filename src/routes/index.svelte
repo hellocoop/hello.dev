3b7db69c5605143513e9b592988cdc733e930b8c
rohanharikr <rohanharikumar80@gmail.com>
2021-12-03 20:48:59 +0400

    Remove artifacts from /public and add root index page

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<main class="py-24 w-full text-center">
+	<h1 class="text-3xl font-semibold">Welcome to Hellō Dev!</h1>
+	<div class="mt-4 space-x-4">
+		<a href="/documentation" class="underline text-blue-800">Documentation</a>
+		<a href="/faqs" class="underline text-blue-800">FAQs</a>
+	</div>
+</main>
