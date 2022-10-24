747a0d4a56882c530280af51234ea84ec122397d
rohanharikr <rohanharikumar80@gmail.com>
2022-10-24 14:25:41 +0400

    fix: send event on hash change

--- src/.vuepress/clientAppEnhance.js
+++ src/.vuepress/clientAppEnhance.js
@@ -1,33 +1,35 @@
-import { defineClientAppEnhance } from '@vuepress/client'
+import { defineClientAppEnhance } from "@vuepress/client"
 
-const sendEvent = async() => {
+const sendEvent = async () => {
   if (localStorage.plausible_ignore) {
     console.warn("Ignoring Event: localStorage flag")
-    return;
+    return
   }
   const _body = {
-      w: window.innerWidth,
-      d: "hello.dev",
-      n: "pageview",
-      r: document.referrer || null,
-      u: window.location.href
-  };
+    w: window.innerWidth,
+    d: "hello.dev",
+    n: "pageview",
+    r: document.referrer || null,
+    u: window.location.href,
+  }
   try {
-      const endpoint = new URL('/api/event', window.location.origin + window.location.pathname)
-      await fetch(endpoint.href, {
-          method: "POST",
-          body: JSON.stringify(_body),
-      });
-      console.info(`Event sent: ${_body.u} (${_body.n})`);
+    const endpoint = new URL(
+      "/api/event",
+      window.location.origin + window.location.pathname
+    )
+    await fetch(endpoint.href, {
+      method: "POST",
+      body: JSON.stringify(_body),
+    })
+    console.info(`Event sent: ${_body.u} (${_body.n})`)
   } catch (err) {
-      console.error(err);
+    console.error(err)
   }
 }
 
 export default defineClientAppEnhance(({ router }) => {
-  if(__VUEPRESS_SSR__) return
+  if (__VUEPRESS_SSR__) return
   router.afterEach((toPage) => {
-    if(toPage.hash) return // ignore anchor links
     sendEvent()
   })
 })
