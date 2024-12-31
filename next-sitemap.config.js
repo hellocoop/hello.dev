104a59f6c28cd4852e8927846ec91da5956b2375
rohanharikr <rohanharikumar80@gmail.com>
2024-12-31 12:39:45 +0000

    add sitemap at build step (#88)

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.hello.dev/',
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+    exclude: ['*/_meta']
+}
